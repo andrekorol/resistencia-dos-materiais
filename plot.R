b0f84249fadb43dbd01a6b32997e2c4fe78b566f
andrekorol <anrobits@yahoo.com.br>
2019-09-12 09:58:17 -0300

    Plot data from all the tests

--- plot.R
+++ plot.R
@@ -1,40 +1,36 @@
 library(ggplot2)
 
-dd <- read.csv("tests/test1.csv", fileEncoding = "UTF-8", header = TRUE,
-                 sep = ",")
+test_files <- list.files("tests", pattern = "*.csv", full.names = TRUE,
+                         recursive = FALSE)
 
-new_colnames <- lapply(colnames(dd), function(name){
-  name <- sub("[:.:]", "(", name)
-  name <- sub("[:.:]", ")", name)
-})
-
-colnames(dd) <- new_colnames
-
-# dd <- data.frame(dt)
-# print(typeof(dd))
-# print(typeof(dt))
-
-x_axis <- sym(colnames(dd)[1])
-y_axis <- sym(colnames(dd)[2])
-
-g <- ggplot(dd, aes(x=!!x_axis, y=!!y_axis)) +
-  geom_line(color="seagreen")
-
-plot(g)
-
-# test_files <- list.files("tests", pattern = "*.csv", full.names = TRUE,
-#                          recursive = FALSE)
-
-subtitles <- c("Cilindro de 7/8\" em diâmetro", 
+titles <- c("Cilindro de 7/8\" em diâmetro", 
                "12 mm de largura x 8 mm de espessura",
                "Cilindro 150 mm de diâmetro",
                "Ensaio de tensão-deformação para um material cerâmico")
 
-i <- 1 # subtitle index
+plot_colors <- c("seagreen", "darkorange", "darkblue", "indianred")
 
-# lapply(test_files, function(test){
-#   dd <- read.table(test, fileEncoding = "UTF-8", header = TRUE, sep = ",")
-#   print(subtitles[i])
-#   print(dd)
-#   i <<- i + 1 # increment the global index variable i
-# })
+i <- 1 # global index
+
+lapply(test_files, function(test){
+  dd <- read.csv(test, fileEncoding = "UTF-8", header = TRUE, sep = ",")
+  new_colnames <- lapply(colnames(dd), function(name){
+    name <- sub(".mm.mm.", "(mm/mm)", name)
+    name <- sub("[:.:]", "(", name)
+    name <- sub("[:.:]", ")", name)
+  })
+  
+  colnames(dd) <- new_colnames
+  
+  x_axis <- sym(colnames(dd)[1])
+  y_axis <- sym(colnames(dd)[2])
+  
+  g <- ggplot(dd, aes(x=!!x_axis, y=!!y_axis)) +
+    geom_line(color=plot_colors[i]) +
+    labs(title=titles[i]) +
+    theme(plot.title = element_text(hjust = 0.5))
+  
+  plot(g)
+  
+  i <<- i + 1 # increment the global index variable i
+})
